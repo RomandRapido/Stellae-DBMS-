a6af962ee9cde5fa4fa09a712fc7f3a7e2fae0cb
RomandRapido <124319961+RomandRapido@users.noreply.github.com>
2023-12-14 00:12:27 +0800

    Update addNew_userData.php

--- signUp/php_modal_connection/addNew_userData.php
+++ signUp/php_modal_connection/addNew_userData.php
@@ -50,13 +50,27 @@
 			$array_columns = implode(',', $array_columns);
 
 			$queryNew = "INSERT INTO users (first_name, last_name, email, user_name, password_hash, education_level_id, school_id, profile_descriptions, program) 
-			VALUES ('$userUpdate[0]', '$userUpdate[1]', '$userUpdate[2]', '$userUpdate[3]', '$userUpdate[4]', $userUpdate[5], $userUpdate[6], '$userUpdate[7]', '$userUpdate[8]')";
-			echo $queryNew;
-			$resultNew = mysqli_query($conn, $queryNew);
+			VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-			if (!$resultNew){die("Insert user info failed");}
+			$stmt = mysqli_prepare($conn, $queryNew);
+
+			if (!$stmt) {
+				die("Error in preparing the statement: " . mysqli_error($conn));
+			}
+
+			mysqli_stmt_bind_param($stmt, "sssssisss", $userUpdate[0], $userUpdate[1], $userUpdate[2], $userUpdate[3], $userUpdate[4], $userUpdate[5], $userUpdate[6], $userUpdate[7], $userUpdate[8]);
+
+			$resultNew = mysqli_stmt_execute($stmt);
+
+			if (!$resultNew) {
+				die("Insert user info failed: " . mysqli_error($conn));
+			}
+
 			echo '<script>alert("Registration Successful")</script>';
 			header('Location: ../../logIn/log_in_page.html');
+
+			mysqli_stmt_close($stmt);
+			mysqli_close($conn);
 		}
 		
 	}
